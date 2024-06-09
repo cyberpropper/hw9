a8ca2cb34b6d31151443ea9a8994760edc8103c5
cyberpropper <osankin2002@mail.ru>
2024-06-09 21:19:22 +0700

    gotovo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Роман";
+        String middleName = "Сергёёвич";
+        String lastName = "Ладошкин";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameTrue.toUpperCase());
 
 
     }
